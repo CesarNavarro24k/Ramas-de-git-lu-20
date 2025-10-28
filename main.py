9c7249d08c1d9d77bf3816b64c219e42da0db873
Cesar <andrescos100@gmail.com>
2025-10-27 20:40:25 -0500

    tablas

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 print("Hola desde main!")
 print("Hola desde rama2!")
+"""
+Hacer un código que haga las tablas de multiplicar del 1 al 10, preguntando que tabla quiere ver
+"""
+numero = int(input("Ingresa el numero del que quieres conocer la tabla:"))
+
+for i in range(1,11):
+    print(f"{numero} X {i} = {numero * i} ")
