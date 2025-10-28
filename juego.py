46719fa13c8348f7a06e005f39841d90e7f62476
larrix14k <andrescos100@gmail.com>
2025-10-28 11:09:35 -0500

    segundo

---
+++ juego.py
@@ -0,0 +1,16 @@
+import random
+
+puntaje = 0
+intentos = 3
+
+while intentos > 0:
+    numero = random.randint(1,10)
+    adi = int(input("Cual es el numero entre 1 y 10:"))
+    if numero == adi:
+        print("lo has adivinado")
+        puntaje += 1
+    else:
+        print("No has adivinado el numero, el numero era:", numero)
+        intentos -= 1
+        print(f"Te quedan {intentos} intentos")
+        print(f"Tu puntaje es: {puntaje} puntos")
